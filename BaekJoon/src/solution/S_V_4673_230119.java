a90bd9ddc7296e99b26eb72563538781a0a6b0e2
sungjoo <hans52410537@gmail.com>
2023-01-19 19:40:34 +0900

    01.19 백준 풀이

--- BaekJoon/src/solution/S_V_4673_230119.java
+++ BaekJoon/src/solution/S_V_4673_230119.java
@@ -11,10 +11,12 @@ public class S_V_4673_230119 {
 
 		ArrayList<Integer> genList = new ArrayList<Integer>();
 		
+		// 제너레이터 숫자를 배열에 넣고 
 		for (int i = 1; i <= 10000; i++) {
 			genList.add(isGen(i));
 		}
 		
+		// 비교
 		for (int i = 1; i <= 10000; i++) {
 			if (!genList.contains(i)) {
 				System.out.println(i);
@@ -22,7 +24,8 @@ public class S_V_4673_230119 {
 		}
 		
 	}
-
+	
+	// generator 함수 
 	public static int isGen(int num) {
 		int tot = num;
 		
