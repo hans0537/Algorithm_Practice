e61d5ab97f3b843268d9bc8c6d66eb71ed2778e9
신성주 <hans52410537@gmail.com>
2023-01-26 14:02:24 +0900

    자바 백준 문제 풀이

--- Java/BaekJoon/src/solution/S_II_14889_230125.java
+++ Java/BaekJoon/src/solution/S_II_14889_230125.java
@@ -5,18 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 14889 스타트와 링크
+// https://www.acmicpc.net/problem/14889
 public class S_II_14889_230125 {
 
 	static int[][] arr;
 	static int min = Integer.MAX_VALUE;
+	static boolean[] check;
+	static int N;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		
 		arr = new int[N][N];
+		check = new boolean[N];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -25,12 +30,57 @@ public class S_II_14889_230125 {
 			}
 		}
 		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println();
+		dfs(0, 0);
+		System.out.println(min);
+	}
+	
+	// cnt는 팀조합 개수
+	public static void dfs(int idx, int cnt) {
+		
+		// 팀이 만들어지면 해당 재귀 종료
+		// 능력치 계산후 최소값 최신화
+		if (cnt == N / 2) {
+			ability();
+			return;
 		}
+		
+		for (int i = idx; i < N; i++) {
+			// 방문 체크
+			if(!check[i]) {
+				check[i] = true;
+				dfs(i + 1, cnt + 1);
+				// 재귀가 끝나면 다시 원상복구
+				check[i] = false;
+			}
+		}
+	}
+	
+	public static void ability() {
+		int start = 0;
+		int link = 0;
+		
+		// 모든 사람 비교
+		for (int i = 0; i < N - 1; i++) {
+			for (int j = i + 1; j < N; j++) {
+				// i 와 j 가 서로 방문 되어 있으면 스타트 팀으로 능력치 추가
+				if(check[i] == true && check[j] == true) {
+					start += arr[i][j] + arr[j][i];
+				}
+				// i 와 j 가 서로 방문 안되어 있으면 링크 팀
+				else if(check[i] == false && check[j] == false) {
+					link += arr[i][j] + arr[j][i];
+				}
+			}
+		}
+		
+		int ans = Math.abs(start - link);
+		
+		// 만약 0 이면 최소값이므로 바로 종료
+		if(ans == 0) {
+			System.out.println(ans);
+			System.exit(0);
+		}
+		min = Math.min(min, ans);
 	}
 
 }
