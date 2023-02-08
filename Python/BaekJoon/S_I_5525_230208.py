e6fc240a5d4abfd3dbaf204f1928f1293efb4e62
sungjoo <hans52410537@gmail.com>
2023-02-08 21:16:40 +0900

    02.08 파이썬 백준 풀이

--- Python/BaekJoon/S_I_5525_230208.py
+++ Python/BaekJoon/S_I_5525_230208.py
@@ -5,11 +5,24 @@ n = int(input())
 m = int(input())
 s = input()
 
+# 조금이라도 시간을 줄이는 방법
+# IOI의 갯수만 체크해서 Pn 따라 갯수만 조정한다
+i, cnt, ans = 0, 0, 0
+while i < m - 1:
+    if s[i:i+3] == 'IOI':
+        cnt += 1
+        # IOI를 찾았으니 인덱스 변경
+        i += 2
+        if cnt == n:
+            cnt -= 1
+            ans += 1
+    else:
+        i += 1
+        cnt = 0
+print(ans)
 
 
-
-
-
+# 서브태스트 1번만 통과 
 # n = int(input())
 # p = 'IO' * n + 'I'
 # # Pn 만들어주기
