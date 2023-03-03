9e07f0baeb6fe0e92e9a640a0ca2845b748e4a63
hans52410537 <hans52410537@gmail.com>
2023-03-03 17:20:15 +0900

    IM 대비 문제풀이

--- Python/BaekJoon/S_V_2628_230303.py
+++ Python/BaekJoon/S_V_2628_230303.py
@@ -2,9 +2,37 @@
 # 2628 종이자르기
 # https://www.acmicpc.net/problem/2628
 
-col, row = map(int, input().split())
+c, r = map(int, input().split())
 N = int(input())
 
+row = []
+col = []
 for i in range(N):
     d, num = map(int, input().split())
+    if d == 0:
+        row.append(num)
+    else:
+        col.append(num)
+
+row.sort()
+col.sort()
+
+rmx = cmx = 0
+tmp = 0
+for i in range(r + 1):
+    if i in row:
+        rmx = max(rmx, i - tmp)
+        tmp = i
+    elif i == r:
+        rmx = max(rmx, i - tmp)
+        
+tmp = 0
+for i in range(c + 1):
+    if i in col:
+        cmx = max(cmx, i - tmp)
+        tmp = i
+    elif i == c:
+        cmx = max(cmx, i - tmp)
+
+print(rmx*cmx)
 
