3969c3bb69b183e3fa18b6d820610d693a77478a
sungjoo <hans52410537@gmail.com>
2023-03-14 22:15:53 +0900

    03.14 백준 문제 풀이

--- Python/BaekJoon/Silver/S_I_1697_230314.py
+++ Python/BaekJoon/Silver/S_I_1697_230314.py
@@ -1,23 +1,27 @@
 # 1697 숨바꼭질
 # https://www.acmicpc.net/problem/1697
+from collections import deque
 
-def dfs(n, k):
-    global ans
+def bfs():
+    q = deque([N])
 
-    if n == K:
-        ans = min(ans, k)
-        return
+    while q:
+        x = q.popleft()
+        if x == K:
+            print(visited[x])
+            break
 
-    if ans <= k:
-        return
+        # bfs 선입선출 큐를 활용하여 가장 먼저 된 숫자를
+        # 되기전 숫자의 횟수를 더해서 최소값을 유지한다
+        # 방문 체크는 최소값 유지 하기 위함
+        for i in (x - 1, x + 1, 2*x):
+            if 0 <= i <= max_v and not visited[i]:
+                visited[i] = visited[x] + 1
+                q.append(i)
 
-    dfs(n + 1, k + 1)
-    dfs(2*n, k + 1)
-    if K < n:
-        dfs(n - 1, k + 1)
 
 N, K = map(int, input().split())
-ans = 1e9
-dfs(N, 0)
-print(ans)
+max_v = 100000 # 최대값을 주어서 무한 반복 하지 않게 하기 위함
+visited = [0] * (max_v + 1)
+bfs()
 
