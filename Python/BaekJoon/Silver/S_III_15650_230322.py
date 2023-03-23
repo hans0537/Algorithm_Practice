17c61a899627c11a4a6b9c2ece91c89992b3438f
신성주 <hans52410537@gmail.com>
2023-03-23 15:49:57 +0900

    03.22 백트래킹 문제풀이

--- Python/BaekJoon/Silver/S_III_15650_230322.py
+++ Python/BaekJoon/Silver/S_III_15650_230322.py
@@ -1,23 +1,23 @@
 # 15650 N과 M(2)
 # https://www.acmicpc.net/problem/15650
-def check(lst):
-    for i in range(len(lst) - 1):
-        for j in range(i + 1, len(lst)):
-            if lst[i] > lst[j]:
-                return False
-    return True
-
-def dfs(n, lst):
-    if n == M:
-        if check(lst):
-            print(*lst)
-        return
-
-    for j in range(1, N + 1):
-        if not visited[j]:
-            visited[j] = 1
-            dfs(n + 1, lst + [j])
-            visited[j] = 0
+# def check(lst):
+#     for i in range(len(lst) - 1):
+#         for j in range(i + 1, len(lst)):
+#             if lst[i] > lst[j]:
+#                 return False
+#     return True
+#
+# def dfs(n, lst):
+#     if n == M:
+#         if check(lst):
+#             print(*lst)
+#         return
+#
+#     for j in range(1, N + 1):
+#         if not visited[j]:
+#             visited[j] = 1
+#             dfs(n + 1, lst + [j])
+#             visited[j] = 0
 
 def dfs(n, lst):
     if n > N:
