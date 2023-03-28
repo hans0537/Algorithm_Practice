bd7bd9de94b318dcbc6286a7ba69194eca844709
신성주 <hans52410537@gmail.com>
2023-03-28 17:50:14 +0900

    03.28

--- Python/BaekJoon/Silver/S_II_6603_230327.py
+++
@@ -1,8 +0,0 @@
-# 6603 로또
-# https://www.acmicpc.net/problem/6603
-
-while True:
-    lst = list(map(int, input().split()))
-    if lst[0] == 0:
-        break
-
---
+++ Python/BaekJoon/Silver/S_II_6603_230328.py
@@ -0,0 +1,20 @@
+# 6603 로또
+# https://www.acmicpc.net/problem/6603
+
+def dfs(idx, cnt, tlst):
+    # 리스트의 첫번째 원소 즉 뽑을 숫자들의 개수를 넘어가면
+    if idx > lst[0]:
+        if cnt == 6:
+            print(*tlst)
+        return
+
+    dfs(idx + 1, cnt + 1, tlst + [lst[idx]])
+    dfs(idx + 1, cnt, tlst)
+
+while True:
+    lst = list(map(int, input().split()))
+    if lst[0] == 0:
+        break
+
+    dfs(1, 0, [])
+    print()
