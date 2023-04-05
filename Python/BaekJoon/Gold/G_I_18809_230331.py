70751d53067a636b45d1bb85376238228e50566a
sungjoo <hans52410537@gmail.com>
2023-04-06 00:11:04 +0900

    .

--- Python/BaekJoon/Gold/G_I_18809_230331.py
+++ Python/BaekJoon/Gold/G_I_18809_230331.py
@@ -8,82 +8,66 @@ def is_valid(x, y):
     return 0 <= x < N and 0 <= y < M and garden[x][y] != 0
 
 # G, R 배양액 퍼트리기
-def bfs(glist, rlist):
-    gq = deque(glist)
-    rq = deque(rlist)
+def bfs(tlist):
+    q = deque()
     v = [[0] * M for _ in range(N)]
 
-    # g이면 -1
-    # r이면 1
-    for i in range(len(glist)):
-        v[glist[i][0]][glist[i][1]] = -1
-
-    for i in range(len(rlist)):
-        v[rlist[i][0]][rlist[i][1]] = 1
-
+    # 가져온거 정보 넣어주기
+    for i in range(len(can_place)):
+        if tlist[i] == 0:
+            continue
+        ti, tj = can_place[i]
+        q.append((ti, tj))
+        v[ti][tj] = tlist[i]
 
     cnt = 0
     # 동일한 시간에 퍼져서 만나면 꽃이 피므로
     # 하나라도 퍼지는게 끝나면 종료
-    while gq and rq:
-        gx, gy = gq.popleft()
-        rx, ry = rq .popleft()
-        # if v[gx][gy] == 10 or v[rx][ry] == 10:
-        #     continue
+    while q:
+        cx, cy = q.popleft()
+
+        if v[cx][cy] == 10: continue
 
         for d in range(4):
-            m_gx = gx + dx[d]
-            m_gy = gy + dy[d]
+            mx = cx + dx[d]
+            my = cy + dy[d]
 
-            if is_valid(m_gx, m_gy):
-                if v[m_gx][m_gy] == 10:
-                    continue
+            if is_valid(mx, my) and v[mx][my] != 10:
 
-                if not v[m_gx][m_gy]:
-                    v[m_gx][m_gy] = v[gx][gy] - 1
-                    gq.append((m_gx, m_gy))
-                # 가려는 곳이 더해졌을때 0 이면
-                elif v[m_gx][m_gy] > 0:
-                    if v[m_gx][m_gy] + v[gx][gy] - 1 == 0:
-                        cnt += 1
-                        v[m_gx][m_gy] = 10
-
-        for d in range(4):
-            m_rx = rx + dx[d]
-            m_ry = ry + dy[d]
-
-            if is_valid(m_rx, m_ry):
-                if v[m_rx][m_ry] == 10:
-                    continue
-
-                if not v[m_rx][m_ry]:
-                    v[m_rx][m_ry] = v[rx][ry] + 1
-                    rq.append((m_rx, m_ry))
-                elif v[m_rx][m_ry] < 0:
-                    if v[m_rx][m_ry] + v[rx][ry] + 1 == 0:
-                        cnt += 1
-                        v[m_rx][m_ry] = 10
-
+                if not v[mx][my]:
+                    if v[cx][cy] < 0:
+                        v[mx][my] = v[cx][cy] - 1
+                    else:
+                        v[mx][my] = v[cx][cy] + 1
+                    q.append((mx, my))
+                # 가려는 곳에 값이 있으면
+                else:
+                    if v[cx][cy] < 0:
+                        if v[mx][my] + v[cx][cy] - 1 == 0:
+                            v[mx][my] = 10
+                            cnt += 1
+                    else:
+                        if v[mx][my] + v[cx][cy] + 1 == 0:
+                            v[mx][my] = 10
+                            cnt += 1
     return cnt
 
 # G, R 배양액 뿌리기 경우의 수
-def dfs(n, gcnt, rcnt, glist, rlist):
+def dfs(n, gcnt, rcnt, tlist):
     global ans
 
     if n == len(can_place):
-        if gcnt == 0 and rcnt == 0:
-            cnt = bfs(glist, rlist)
+        if gcnt == G and rcnt == R:
+            cnt = bfs(tlist)
             ans = max(ans, cnt)
         return
 
     # G 선택
-    if gcnt > 0:
-        dfs(n + 1, gcnt - 1, rcnt, glist + [can_place[n]], rlist)
+    dfs(n + 1, gcnt + 1, rcnt, tlist + [1])
     # R 선택
-    if rcnt > 0:
-        dfs(n + 1, gcnt, rcnt - 1, glist, rlist + [can_place[n]])
+    dfs(n + 1, gcnt, rcnt + 1, tlist + [-1])
     # 선택 X
-    dfs(n + 1, gcnt, rcnt, glist, rlist)
+    dfs(n + 1, gcnt, rcnt, tlist + [0])
 
 N, M, G, R = map(int, input().split())
 garden = []
@@ -96,5 +80,5 @@ for i in range(N):
     garden.append(tmp)
 
 ans = 0
-dfs(0, G, R, [], [])
+dfs(0, 0, 0, [])
 print(ans)
