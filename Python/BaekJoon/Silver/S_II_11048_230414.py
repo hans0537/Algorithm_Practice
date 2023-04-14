a53926344aa8365e8c5a292ffb96cb4af452864c
hans0537 <hans52410537@gmail.com>
2023-04-14 21:49:34 +0900

    04.14 백준 dp 문제풀이 완

--- Python/BaekJoon/Silver/S_II_11048_230414.py
+++ Python/BaekJoon/Silver/S_II_11048_230414.py
@@ -8,19 +8,22 @@ dp = [[0] * M for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
-
+        tmp = arr[i][j]
         # 왼쪽에서 오는 경우
         if j - 1 >= 0:
-            arr[i][j] = max(arr[i][j], arr[i][j] + arr[i][j - 1])
+            tmp = max(tmp, arr[i][j] + dp[i][j - 1])
 
         # 위에서 오는 경우
         if i - 1 >= 0:
-            arr[i][j] = max(arr[i][j], arr[i][j] + arr[i - 1][j])
+            tmp = max(tmp, arr[i][j] + dp[i - 1][j])
 
         # 왼쪽 위 대각선에서 오는 경우
         if i - 1 >= 0 and j - 1 >= 0:
-            arr[i][j] = max(arr[i][j], arr[i][j] + arr[i - 1][j - 1])
+            tmp = max(tmp, arr[i][j] + dp[i - 1][j - 1])
 
-print(arr[N - 1][M - 1])
+        # dp테이블에 넣어준다
+        dp[i][j] = tmp
+
+print(dp[N - 1][M - 1])
 
 
