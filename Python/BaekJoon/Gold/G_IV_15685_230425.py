b7bc48e483ce9b9441b63e3b1aa42716e31eda72
sungjoo <hans52410537@gmail.com>
2023-04-26 00:04:07 +0900

    04.25 백준 문제 풀이 완

--- Python/BaekJoon/Gold/G_IV_15685_230425.py
+++ Python/BaekJoon/Gold/G_IV_15685_230425.py
@@ -5,51 +5,48 @@ dx = [0, -1, 0, 1]
 dy = [1, 0, -1, 0]
 
 def bfs(i, j, d, g):
-    # 현재 길이
-    l = 0
     # 현재 위치
     cx = i
     cy = j
 
+    dlist = [d]
     # 세대 만큼 이동
     while g > 0:
-        if l == 0:
+        for k in range(len(dlist) - 1, -1, -1):
             arr[cx][cy] = 1
 
+            # 방향 전환
+            # 규칙에 의해 전 세대들을 거꾸로부터 1을 더한게 현재 방향
+            # 방향 전환후 리스트 최신화
+            cd = (dlist[k] + 1) % 4
+            dlist.append(cd)
+
             # 현재 방향으로 한칸 이동
-            mx = cx + dx[d]
-            my = cy + dy[d]
+            mx = cx + dx[cd]
+            my = cy + dy[cd]
             arr[mx][my] = 1
 
-            # 끝점에서 시계 방향 전환
+            # 끝점으로 최신화
             cx = mx
             cy = my
-            d = (d + 1) % 4
 
-            l += 1
-        else:
-            for _ in range(l):
-                arr[cx][cy] = 1
-
-                # 현재 방향으로 한칸 이동
-                mx = cx + dx[d]
-                my = cy + dy[d]
-                arr[mx][my] = 1
-
-                # 끝점에서 시계 방향 전환
-                cx = mx
-                cy = my
-                d = (d + 1) % 4
-
-            l += l
+        # 세대 감소
         g -= 1
 
 
 N = int(input())
-arr = [[0] * 10 for _ in range(10)]
+arr = [[0] * 101 for _ in range(101)]
 
 for _ in range(N):
     y, x, d, g = map(int, input().split())
-    bfs(x, y, d, g)
+    # 0세대 처리
+    arr[x][y] = 1
+    arr[x+dx[d]][y+dy[d]] = 1
+    bfs(x+dx[d], y+dy[d], d, g)
 
-print(arr)
+ans = 0
+for i in range(100):
+    for j in range(100):
+        if arr[i][j] and arr[i+1][j] and arr[i][j+1] and arr[i+1][j+1]:
+            ans += 1
+print(ans)
